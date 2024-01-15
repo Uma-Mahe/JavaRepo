99905c4d20b23e37601faf81b6758808576148c0
Uma-Mahe <uma6mahe@gmail.com>
2024-01-15 11:02:10 -0600

    Swap Value with third variable

---
+++ FirstJavaProject/src/FirstPackage/SwapValueWithThirdVar.java
@@ -0,0 +1,15 @@
+package FirstPackage;
+
+public class SwapValueWithThirdVar {
+	public static void main(String[] args) {
+		int temp;
+		int i =10;
+		int j = 7;
+		temp = i;
+		i = j;
+		j = temp;
+		System.out.println(i);
+		System.out.println(j);		
+	}
+
+}
