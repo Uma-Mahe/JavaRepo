12e0b12dcddd3dc94fd63cc9d29bdeececc4d781
Uma-Mahe <uma6mahe@gmail.com>
2024-01-18 12:43:49 -0600

    Time Conversion

--- FirstJavaProject/src/FirstPackage/TimeConversion.java
+++ FirstJavaProject/src/FirstPackage/TimeConversion.java
@@ -5,14 +5,18 @@ import java.util.Scanner;
 public class TimeConversion {
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
+		
+		// Enter the second
 		System.out.println("Enter the second : ");
 		int second = scn.nextInt();
+		
+		//conversion of second to hours minutes seconds
 		int sec = second%60;
 		int min1 = second/60;
 		int min = min1%60;
 		int hour = min1/60;
-
-		System.out.println(hour + " hour " + min + " minutes " + sec + " seconds ");
+		
+		System.out.println(hour + " hours " + min + " minutes " + sec + " seconds ");
 
 	}
 }
