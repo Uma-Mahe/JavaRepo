06324ae5607471f332c7df9602b0bf8839a7cdf3
Uma-Mahe <uma6mahe@gmail.com>
2024-01-18 13:56:33 -0600

    Arithmetic Calculation

---
+++ FirstJavaProject/src/FirstPackage/ArithmeticCalculation.java
@@ -0,0 +1,29 @@
+package FirstPackage;
+
+import java.util.Scanner;
+import java.math.*;
+public class ArithmeticCalculation {
+public static void main(String[] args) {
+	Scanner scn = new Scanner(System.in);
+	System.out.println("Enter the integer A :");
+	int A = scn.nextInt();
+	System.out.println("Enter the integer B :");
+	int B = scn.nextInt();
+	
+	// Calculation of sum, difference, product, average, distance, max and min
+	int sum = A + B;
+	int difference = A - B;
+	int product = A*B;
+	int average = (A + B)/2;
+	int distance = Math.abs(A - B);
+	int max = Math.max(A, B);
+	int min = Math.min(A, B);
+	System.out.println("sum : "+sum);
+	System.out.println("Difference : "+ difference);
+	System.out.println("Product : "+ product);
+	System.out.println("Average : "+ average);
+	System.out.println("Distance : "+ distance);
+	System.out.println("Maximum : "+ max);
+	System.out.println("Minimum : "+ min);
+}
+}
