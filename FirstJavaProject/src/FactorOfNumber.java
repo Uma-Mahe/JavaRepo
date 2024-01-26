b421e464e48b9cd3488f81197e5b9ed31599f982
Uma-Mahe <uma6mahe@gmail.com>
2024-01-26 00:01:47 -0600

    FactorOfNumber

---
+++ FirstJavaProject/src/FactorOfNumber.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class FactorOfNumber {
+	public static void main(String[] args) {
+		
+		Scanner scn = new Scanner(System.in);
+		
+		System.out.print("Enter a Number : ");
+		int value = scn.nextInt();
+		
+		for (int i = 1; i <= value ; i++) {
+			if ((value % i) == 0) {
+				System.out.println("Factor of the Number : " + i);
+			}
+		}
+	}
+
+}
