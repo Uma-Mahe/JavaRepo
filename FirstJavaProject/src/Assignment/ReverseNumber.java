22aea510a58b17cdcab819adf5b777989af0b300
Uma-Mahe <uma6mahe@gmail.com>
2024-01-30 12:17:23 -0600

    Reverse Number Update

--- FirstJavaProject/src/Assignment/ReverseNumber.java
+++ FirstJavaProject/src/Assignment/ReverseNumber.java
@@ -7,19 +7,19 @@ public class ReverseNumber {
 		
 		System.out.print("Enter an Integer : ");
 		int num = scn.nextInt();
-		int digit, num1 = 0;
+		int digit, revNumber = 0;
 		
 		while (num != 0) {
 			digit = num % 10;
 			//System.out.println(digit);
 			
-			num1 = num1*10 + digit;
+			revNumber = revNumber*10 + digit;
 			//System.out.println(num1);
 			
 			num = num/10;
 			//System.out.println(num);
 			
 		}
-		System.out.println("Reverse Number : " + num1);
+		System.out.println("Reverse Number : " + revNumber);
 	}
 }
