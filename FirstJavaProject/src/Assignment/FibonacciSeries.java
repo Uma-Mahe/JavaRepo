fe6f0813506b3ab26e80b78270be8ebacdff71ac
Uma-Mahe <uma6mahe@gmail.com>
2024-01-30 12:26:41 -0600

    Update Fibonacci Series

--- FirstJavaProject/src/Assignment/FibonacciSeries.java
+++ FirstJavaProject/src/Assignment/FibonacciSeries.java
@@ -1,7 +1,13 @@
 package Assignment;
 
+import java.util.Scanner;
+
 public class FibonacciSeries {
 	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		System.out.print("Enter the number of fibonacci series : ");
+		int number = scn.nextInt();
+		
 		int i = 0;
 		int j = 1;
 		int k = 0;
@@ -9,12 +15,11 @@ public class FibonacciSeries {
 		System.out.print(i + " ");
 		System.out.print(j + " ");
 		
-		for (int count = 1; count <= 10; count++) {
+		for (int count = 1; count <= number-2; count++) {
 			k = i + j;
 			System.out.print(k + " ");
 			i = j;
 			j = k;
 		}
 	}
-
 }
