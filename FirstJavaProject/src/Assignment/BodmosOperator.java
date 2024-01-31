2a182f7bc744d7a733f9975112c1883d6234fef3
Uma-Mahe <uma6mahe@gmail.com>
2024-01-31 10:44:56 -0600

    Bodmos Operator

---
+++ FirstJavaProject/src/Assignment/BodmosOperator.java
@@ -0,0 +1,9 @@
+package Assignment;
+
+public class BodmosOperator {
+	public static void main(String[] args) {
+		int i=6, j=1, k=2, m=7;
+		int result = ((i-j)*k)+m;
+		System.out.println(result);
+	}
+}
