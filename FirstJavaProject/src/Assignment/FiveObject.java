9a2d9ba59889fdb0ebf653ae5f27e41c636d6698
Uma-Mahe <uma6mahe@gmail.com>
2024-02-06 11:40:48 -0600

    Five Objects

---
+++ FirstJavaProject/src/Assignment/FiveObject.java
@@ -0,0 +1,27 @@
+package Assignment;
+
+public class FiveObject {
+	public static void main(String[] args) {
+		A1 obj1 = new A1();
+		A2 obj2 = new A2();
+		A3 obj3 = new A3();
+		A4 obj4 = new A4();
+		A5 obj5 = new A5();
+	}
+
+}
+class A1 {
+	
+} 
+class A2 {
+	
+}
+class A3 {
+	
+}
+class A4 {
+	
+}
+class A5 {
+	
+}
