086eb442358e96dca0642fd3e6b3f7c11014a524
Uma-Mahe <uma6mahe@gmail.com>
2024-03-07 11:02:35 -0600

    Comparison of Strings

---
+++ FirstJavaProject/src/Assignment/DiffBetweenEqualsString.java
@@ -0,0 +1,12 @@
+package Assignment;
+
+public class DiffBetweenEqualsString {
+	public static void main(String[] args) {
+		String str1 = new String("Session");
+		String str2 =  new String("Session");
+		
+		System.out.println(str1==str2);
+		System.out.println(str1.equals(str2));		
+	}
+
+}
